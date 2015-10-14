256d2ef109d4d86e01da49acee08179554245ae6
carnifun <mohamed.elassadi@gmail.com>
2015-10-15 01:00:07 +0200

    done

--- client.rb
+++ client.rb
@@ -1,8 +1,49 @@
 module RubyLcd
   class Client
     require 'socket'
-
+   def self.send request 
+      socket = TCPSocket.open("127.0.0.1","2010")
+      socket.print(request)               # Send request
+      socket.close
+    end
+    #def self.method_missing(method_name, *arguments, &block)
+    #  puts "needs this class method#{method_name}"
+    #end
+    
+    def self.warning      
+      send "{\"command\":\"warning\"}\r\n"
+    end
+    def self.okay      
+      send "{\"command\":\"okay\"}\r\n"
+    end
+    def self.error      
+      send "{\"command\":\"error\"}\r\n"
+    end
+    def self.action      
+      send "{\"command\":\"action\"}\r\n"
+    end
+    def self.off      
+      send "{\"command\":\"all_off\"}\r\n"
+    end
+    def self.led(command)
+      send "{\"command\":\"#{command}\"}\r\n"
+    end
+     def self.funny
+      send "{\"command\":\"funny\", \"infinity\":\"true\"}\r\n"
+    end
+    
     def self.single_line(msg="singel line message ffrom client ", row = 1 )
+
+      request = "{\"text\":\"#{msg}\", \"single_line\":\"#{row}\" }\r\n"
+
+      socket = TCPSocket.open("127.0.0.1","2000")
+      socket.print(request)               # Send request
+      #response = socket.read              # Read complete response
+      # Split response at first blank line into headers and body
+      socket.close
+      #puts response
+    end
+    def self.beuteln(msg="das ist ein Beutel", row = 1 )
 
       request = "{\"text\":\"#{msg}\", \"single_line\":\"#{row}\" }\r\n"
 
--- heat_controller.rb
+++ heat_controller.rb
@@ -1,6 +1,17 @@
+class String
+      def to_16
+        self + " " * (16 - size)
+      end
+  end
+
 module HeatController
+  
   require "wiringpi"
   APP_ROOT = File.dirname(__FILE__)
+  HIGH = 1 
+  LOW = 0    
+  T_MS = 1.0000000/1000000
+  MAIN_LOOP_INTERVALL = 10
   class << self
   def log(msg)
     Logger.log(msg)
@@ -9,6 +20,7 @@ module HeatController
     Logger.log_error(msg)
   end
   end
+  
   class Logger
     class << self
       @@initialized = false        
@@ -29,6 +41,28 @@ module HeatController
     end 
     
   end
+  
+  class Led
+    require 'socket'
+    def self.tcp_send request 
+      socket = TCPSocket.open("127.0.0.1","2010") 
+      socket.print(request)               # Send request
+      socket.close
+    end
+    
+    
+    def self.method_missing(method_name, *arguments, &block)
+      tcp_send "{\"command\":\"#{method_name}\"}\r\n"
+    end
+  
+    def self.loading      
+      tcp_send "{\"command\":\"funny\", \"infinity\":\"true\"}\r\n"
+    end       
+    def self.fatal_error
+      tcp_send "{\"command\":\"error\", \"infinity\":\"true\"}\r\n"
+    end
+  end
+  
   class Lcd
     require 'socket'
     def self.sline(msg, row = 1 )
@@ -58,16 +92,17 @@ module HeatController
       socket.close
     end    
   end
+
+  
   class RelaisCard
     # Wiring pins 
     # model B+ 
-    # CHANNES ar numbered from 1 to 4 
-    CHANNES = [ 3, 12, 13, 14 ]
+    # CHANNELS ar numbered from 1 to 4 
+    # CHANNELS = [ 3, 12, 13, 14 ]
     # model A+ 
-    #CHANNES = [-1,  21 , 22, 23, 24 ]
+    CHANNELS = [21 , 22, 23, 24 ]
     @@initialized = false
-    HIGH = 1 
-    LOW = 0    
+
     class << self
       
       def initialized?
@@ -75,23 +110,26 @@ module HeatController
       end
       def init
         @@initialized = true
-        CHANNES.each do | c |
+        CHANNELS.each do | c |
           Wiringpi.pinMode(c, 1)
           Wiringpi.digitalWrite(c, HIGH)
         end
       end  
       def get_state(actuator)
-        Wiringpi.digitalRead(actuator[:channel].to_i + 1)        
+        pin = CHANNELS[actuator[:channel].to_i - 1]
+        Wiringpi.digitalRead(pin)        
       end    
       def action (channel, a)
+        puts "channel #{channel} action #{a}"
         return if channel.nil? or channel.empty?
         channel = channel.to_i  - 1 
         return if  channel > 3 or channel < 0 
-        pin = CHANNES[channel]
+        pin = CHANNELS[channel]
+        puts "Pin #{pin}"
         old_state = Wiringpi.digitalRead(pin)
         new_state = (a=="on") ? LOW : HIGH
+        puts "action #{a} old_state#{old_state} new_state#{new_state} -- "
         if old_state.to_i != new_state.to_i
-          puts "action #{a} old_state#{old_state} new_state#{new_state} -- "
           Wiringpi.digitalWrite(pin, new_state)
           HeatController.log("Kanal #{channel}  wurde auf #{a} gesetzt" )
           true
@@ -99,7 +137,10 @@ module HeatController
         false
       end
       def on(channel) 
-        action(channel, "on")
+        [1,2,3,4].each do |  c | 
+          action(c.to_s, "on")
+        end
+        #action(channel, "on")
       end
       def off(channel) 
         action(channel, "off")
@@ -114,11 +155,12 @@ module HeatController
       def read_config_file
           @config = json_from_file(File.join(APP_ROOT, "config","config.json")) 
           if @config.nil? 
-            Lcd.mlines("Konfiguration konnte nicht gefunden werden , versuche default Configuration zu laden")          
-            sleep(2)
+            Lcd.mlines("lade default".to_16 + "Configuration")          
+            sleep(5)
             @config = json_from_file(File.join(APP_ROOT, "config","config.default.json")) 
             if @config.nil?
-              Lcd.mlines("Keine Konfiguration gefunden Programm wird gestoppt")
+              Lcd.mlines("Keine Konfiguration".to_16+  "gefunden. exit")
+              Led.fatal_error
               exit()
             end
           end
@@ -141,7 +183,7 @@ module HeatController
         md5 = Digest::MD5.hexdigest(content)
         if File.exists?(File.join(APP_ROOT, "config",md5))       
           Lcd.mlines("Konfiguration ist breits aktuell")
-          sleep(2)
+          sleep(3)
           return  
         end
         if json_from_content(content).nil?
@@ -182,10 +224,8 @@ module HeatController
             Lcd.mlines("config.json     NICHT gefunden!")           
            end
          else
-           puts "was hier "
            Lcd.mlines("Kein Usb erkannt")
            sleep(5)           
-           puts " was hier 2 "
          end
          false
         end    
@@ -209,33 +249,33 @@ module HeatController
          end
          t.to_f/1000          
       end
-      def update_status_line
+      def update_status
         config = ConfigReader.config
         status =" " 
         config[:sensors].each do | s |
-          status +="#{s[:name]}:#{@sensor_data[s[:id]]} "
+          status +="#{s[:name]}:#{@sensor_data[s[:id]].to_i} C".to_16          
         end
         
         config[:actuators].each do | actuator |
           state = RelaisCard.get_state(actuator)
-          status +="#{actuator[:name]}:#{state==1?"Aus":"An"} "
+          puts "state #{state} == 1 "
+          status +="#{actuator[:name]}:#{state==1?"Aus":"An"}".to_16
         end
-        Lcd.sline(status, 1)
+        Lcd.mlines(status)
       end
       
       def read_temperature
         config = ConfigReader.config
+        return_value =  true
         @sensor_data={}
         config[:sensors].each do |s |
           @sensor_data[s[:id]] = read_sensor_temperatur(s[:id])
           if @sensor_data[s[:id]]<1
-            Lcd.mlines("Achtung Sensor #{s[:name]} ist Ausgefallen")
-            sleep(3)
-          else
-            Lcd.sline("S#{s[:name]}:#{sprintf('%.2f',@sensor_data[s[:id]])} C",2)
-            sleep(1)
+            Lcd.mlines("Achtung ".to_16 + "#{s[:name]} defekt!")
+            return_value =  false
           end
         end
+        return_value
       end
       def get_sensor (s_id)
        config[:sensors].each do |s |
@@ -245,7 +285,7 @@ module HeatController
       def wait_for_lcd_server
         loop do
           begin 
-            Lcd.mlines("Server wird gestartet")
+            Lcd.sline("Lcd server up")
             sleep(2)
             return true
           rescue
@@ -271,9 +311,6 @@ module HeatController
           "( #{c} )"
         end.join(und ?  " and " : " or ") if rule_conditions
       end
-      
-      
-      
       def rule_fullfilled ( rule )
         # prepare the conditions 
         and_condition = compose_condition_from_rule(rule) 
@@ -283,7 +320,7 @@ module HeatController
         rule_condition = [and_condition, or_condition].compact.join(" or ") 
         puts " MEIN CONDITON "
         puts rule_condition
-        sleep(1)
+        #sleep(1)
         if (eval " (#{rule_condition}) ? true : false ")
           puts "Condition ist OKAY "
           # save rule temperature
@@ -291,21 +328,15 @@ module HeatController
         end
         false        
       end
-      
       def perform_action (actuator, action)
-        puts "performing action#{action}"
+        #puts "performing action#{action}"
         excecuted = RelaisCard.send(action, actuator[:channel])
         if excecuted
           Lcd.sline("#{actuator[:name]} = #{action}")
-          puts "action#{action} executed "
-          
+          puts "action#{action} executed "          
           sleep(1)
         end
-        update_status_line
       end
-
-      
-
       def init
         # wait for lcd server to go up
         Logger.init
@@ -313,23 +344,29 @@ module HeatController
         ConfigReader.read_config_file
         ConfigReader.reload_config if ConfigReader.detect_usb_drive         
         Wiringpi.wiringPiSetup
-        RelaisCard.init        
+        RelaisCard.init       
       end      
-      
       def run
         # main loop
+        Led.loading
         init
         config = ConfigReader.config
         loop do
-          read_temperature
-        # get the rules 
-          config[:actuators].each do | actuator |
-            actuator[:rules].each do | rule |
-              perform_action(actuator, rule[:action]) if rule_fullfilled(rule)
-            end          
+          if read_temperature
+            Led.okay
+            # get the rules 
+            config[:actuators].each do | actuator |
+              actuator[:rules].each do | rule |
+                perform_action(actuator, rule[:action]) if rule_fullfilled(rule)
+              end          
+            end
+          else
+            Led.error
+            #wait for error message to be shown 
+            sleep(5)  
           end
-	        puts " In the main loop 5s waiting"	
-          sleep(5)
+          update_status
+          sleep(MAIN_LOOP_INTERVALL)
         end        
       end  
     end
